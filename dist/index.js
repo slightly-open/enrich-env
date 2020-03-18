0dd55bad7daa7dff4f2cb7a25ed98a7c80ee143e
zaverden <zaverden@gmail.com>
2020-03-18 17:55:21 +0700

    fix env-prefix param name

--- dist/index.js
+++ dist/index.js
@@ -7558,7 +7558,7 @@ function getBranch({ repo }) {
     }
     const branch = GITHUB_REF.slice(BRANCH_PREFIX.length);
     const branchSlag = nameSanitized(branch);
-    const branchTreeUrl = url(branchSlag, repo.owner, repo.repo);
+    const branchTreeUrl = url(branch, repo.owner, repo.repo);
     names.set('BRANCH', branch);
     names.set('BRANCH_SLAG', branchSlag);
     names.set('BRANCH_TREE_URL', branchTreeUrl);
